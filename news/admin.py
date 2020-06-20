d0f80e40fed27acff7610f9799d04c1bc2d3fbbf
Rafal Przetakowski <rafal.przetakowski@theonequestion.com>
2020-06-20 12:50:38 +0200

    Categories and news

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Modyfikacja podstawowego zachowania widoku w panelu administracji."""
+
+    @staticmethod
+    def related_categories_number(obj):
+        return obj.children.count()
+
+    readonly_fields = ('id',)
+    list_display = ('name', 'parent_category', 'related_categories_number')
+
+
+@admin.register(models.News)
+class NewsAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    list_display = ('title', 'category')
+
+
+
+# admin.site.register(models.News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,28 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class News(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=150, null=False, blank=False)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'News'
