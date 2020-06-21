e859a7f8f06e2a609723241cdfbfd651f7be3590
Rafal Przetakowski <rafal.przetakowski@theonequestion.com>
2020-06-21 12:05:37 +0200

    own widget

---
+++ news/form_widgets.py
@@ -0,0 +1,28 @@
+"""copyright (c) 2020 Beeflow Ltd.
+
+Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
+
+from django import forms
+
+
+class ListTextWidget(forms.TextInput):
+    def __init__(self, name, data_list, *args, **kwargs):
+        super(ListTextWidget, self).__init__(*args, **kwargs)
+        self._name = name
+        self._list = data_list
+
+        self.attrs.update({'list': 'list_%s' % self._name})
+
+    def render(self, name, value, attrs=None, renderer=None):
+        text_html = super(ListTextWidget, self).render(name, value, attrs=attrs, renderer=renderer)
+        # text_html += '<datalist id="list_%s">' % self._name
+        #
+        # for item in self._list:
+        #     text_html += f'<option value="{item}">'
+        #
+        # text_html += '</datalist>'
+        #
+        # return text_html
+
+        return text_html + '<datalist id="list_%s">' % self._name + ''.join(
+            ['<option value="%s">' % item for item in self._list]) + '</datalist>'
--- news/forms/newsform.py
+++ news/forms/newsform.py
@@ -3,10 +3,37 @@
 Author Rafal Przetakowski <rafal.p@beeflow.co.uk>"""
 from django import forms
 
-from news.models import News
+from news.form_widgets import ListTextWidget
+from news.models import News, Category
 
 
 class NewsForm(forms.ModelForm):
+    title = forms.CharField(
+        label='Tytuł newsa',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control form-control-lg pr-4 shadow-none',
+                'placeholder': 'Tytuł'
+            }
+        )
+    )
+
+    content = forms.CharField(
+        label='Treść newsa',
+        widget=forms.Textarea(
+            attrs={'class': 'form-control form-control-lg pr-4 shadow-none', }
+        )
+    )
+
+    category = forms.CharField(
+        label='Kategoria',
+        widget=ListTextWidget(
+            name="category",
+            data_list=Category.objects.all(),
+            attrs={'class': 'form-control form-control-lg pr-4 shadow-none', }
+        )
+    )
+
     class Meta:
         model = News
         fields = ('title', 'content', 'category')
